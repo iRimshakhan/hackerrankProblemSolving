184cc3efa5de338c15c1d8e92f568ecff250d12c
iRimshakhan <150517236+iRimshakhan@users.noreply.github.com>
2023-12-13 20:12:51 +0400

    python division

---
+++ division.py
@@ -0,0 +1,16 @@
+#Task
+#The provided code stub reads two integers,  and , from STDIN.
+#Add logic to print two lines. The first line should contain the result of integer division,  a//b . The second line should contain the result of float division, a /b .
+#No rounding or formatting is necessary.
+
+def division1(a,b):
+        return a//b
+def division(a,b):
+        return a/b
+a = int(input())
+b = int(input())
+divi= division1(a,b)
+print(divi)
+divide= division(a,b)
+print(divide)
+
